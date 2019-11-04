e22da7199e5c125713ec153c576eae43c8339e87
neotamoe <neota.moe@gmail.com>
2019-11-04 10:30:11 -0600

    fix navigation to work in functional component; modify signup

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,21 +1,44 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import { StatusBar, SafeAreaView, View, Text, Button, StyleSheet } from 'react-native';
+import { Input } from 'react-native-elements';
 
-class SignUp extends Component {
-  render() {
+const SignUp = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     return (
       <>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView style={styles.container}>
           <Text style={styles.header}>SignUp Page</Text>
+          <Input
+            value={email}
+            placeholder='Email'
+            // errorStyle={styles.inputError}
+            // errorMessage='Enter a valid email address'
+            onChangeText={(value) => setEmail(value)}
+          />
+          <Input
+            value={password}
+            placeholder='Password'
+            // errorStyle={styles.inputError}
+            // errorMessage='Password must be at least 4 characters'
+            onChange={(e) => setPassword(e.nativeEvent.text)}
+          />
+          <Input
+            value={confirmPassword}
+            placeholder='Confirm Password'
+            // errorStyle={styles.inputError}
+            // errorMessage='Password must be at least 4 characters'
+            onChange={(e) => setConfirmPassword(e.nativeEvent.text)}
+          />
           <Button 
             title="Already Signed Up?  Click to Log In." 
-            onPress={() => this.props.navigation.navigate('Login')}
+            onPress={() => navigation.navigate('Login')}
           />
         </SafeAreaView>
       </>
     )  
-  }
 }
 
 const styles = StyleSheet.create({
